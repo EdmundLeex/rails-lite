a21c761fb019fab5801d1cd0c67268fcfce83ddf
Edmund Li <edmund.xz.lee@gmail.com>
2015-10-03 15:09:00 -0700

    refactored flash into own file, now flash, flash.now working

---
+++ lib/flash.rb
@@ -0,0 +1,42 @@
+class Flash
+  def initialize(req)
+    cookies = req.cookies.find { |cook| cook.name == '_rails_lite_app_flash' }
+    # debugger
+    if cookies.nil?
+      @flash = { "showed" => false }
+    else
+      cookie_flash = JSON.parse(cookies.value)
+      # debugger
+      if cookie_flash.nil?
+        @flash = { "showed" => false }
+      else
+        # debugger
+        @flash = cookie_flash
+      end
+    end
+    # debugger
+  end
+
+  def [](key)
+    @flash[key.to_sym] || @flash[key.to_s]
+  end
+
+  def []=(key, val)
+    @flash[key.to_sym] = val
+  end
+
+  def store_flash(res)
+    # debugger
+    if @flash["showed"]
+      @flash = { "showed" => false }
+    else
+      @flash["showed"] = true
+    end
+
+    res.cookies << WEBrick::Cookie.new('_rails_lite_app_flash', @flash.to_json)
+  end
+
+  def now
+    @flash
+  end
+end
--- lib/session.rb
+++ lib/session.rb
@@ -1,8 +1,3 @@
-require 'byebug'
-
-require 'json'
-require 'webrick'
-
 class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
@@ -36,48 +31,5 @@ class Session
   def store_session(res)
     # debugger
     res.cookies << WEBrick::Cookie.new('_rails_lite_app_session', @session.to_json)
-  end
-end
-
-class Flash
-  def initialize(req)
-    cookies = req.cookies.find { |cook| cook.name == '_rails_lite_app_flash' }
-    # debugger
-    if cookies.nil?
-      @flash = { "showed" => false }
-    else
-      cookie_flash = JSON.parse(cookies.value)
-      # debugger
-      if cookie_flash.nil?
-        @flash = { "showed" => false }
-      else
-        # debugger
-        @flash = cookie_flash
-      end
-    end
-    # debugger
-  end
-
-  def [](key)
-    @flash[key]
-  end
-
-  def []=(key, val)
-    @flash[key] = val
-  end
-
-  def store_flash(res)
-    # debugger
-    if @flash["showed"]
-      @flash = { "showed" => false }
-    else
-      @flash["showed"] = true
-    end
-
-    res.cookies << WEBrick::Cookie.new('_rails_lite_app_flash', @flash.to_json)
-  end
-
-  def now
-    @flash
   end
 end
