e96913f44ef0d889f1648960611c39bd239e0bca
Edmund Li <edmund.xz.lee@gmail.com>
2015-11-09 22:15:38 -0800

    changed signup redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,9 @@ class UsersController < ApplicationController
     @user.password = params[:user][:password]
 
     if @user.save
-      redirect_to "/user/#{@user.id}"
+      login(@user)
+      redirect_to "/tasks"
+      # redirect_to "/user/#{@user.id}"
     else
       flash.now["errors"] = "Oops... Something has gone wrong."
       render :new
